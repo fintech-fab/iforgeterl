fced72efc9de5bf402c77ecbf27df1bb0c708641
Michael Masin <masin_m@itar-tass.com>
2014-06-21 17:30:43 +0400

    redo notice model

--- src/notice.erl
+++ src/notice.erl
@@ -9,20 +9,52 @@
 -module(notice).
 -author("mart").
 
-%% API
--export([get_notices/2]).
+%% APIa
+-export([get/1, add/1, get_all/0, remove/1]).
+-define(PREFIX, "notice:").
 
+add({notice,Group, Datetime, Text}) ->
+    {ok, RedisConnection} = eredis:start_link(),
+    NoticeUuid = uuid:to_string(uuid:uuid4()),
+    Author = "default",
+    {ok, <<"OK">>} = eredis:q(
+        RedisConnection,
+        ["HMSET", ?PREFIX ++ NoticeUuid, "group", Group, "message", Text, "datetime", Datetime, "author", Author]
+    ),
+    list_to_binary(NoticeUuid).
 
-get_notices([H | T], RedisConnection) ->
-    {ok, Notice} = eredis:q(RedisConnection, ["GET", H]),
-        "notice:" ++ Uuid = binary_to_list(H),
+get({notice_uuid, Uuid}) ->
+    {ok, RedisConnection} = eredis:start_link(),
+    {
+        ok,
+        [
+            <<"group">>, Group,
+            <<"message">>, Message,
+            <<"datetime">>, Datetime,
+            <<"author">>, Author
+        ]
+    } = eredis:q(RedisConnection, ["HGETALL", ?PREFIX ++ Uuid]),
 
     [
-        {
-            {<<"uuid">>, list_to_binary(Uuid)},
-            {<<"notice">>, Notice}
-        }
-        | get_notices(T, RedisConnection)
-    ];
-get_notices([], _) ->
+        {<<"uuid">>, list_to_binary(Uuid)},
+        {<<"group">>, Group},
+        {<<"message">>, Message},
+        {<<"datetime">>, Datetime},
+        {<<"author">>, Author}
+    ].
+
+get_all() ->
+    {ok, RedisConnection} = eredis:start_link(),
+    {ok, Keys} = eredis:q(RedisConnection, ["KEYS", ?PREFIX ++ "*"]),
+    get_notices_by_keys(Keys).
+
+remove({notice_uuid, <<Uuid>>}) ->
+    {ok, RedisConnection} = eredis:start_link(),
+    {ok, <<"OK">>} = eredis:q(RedisConnection, ["DEL", ?PREFIX ++ binary_to_list(Uuid)]),
+    ok.
+
+get_notices_by_keys([Key | Keys]) ->
+        ?PREFIX ++ <<Uuid>> = binary_to_list(Key),
+    [?MODULE:get({notice, Uuid}) | ?MODULE:get_notices_by_keys(Keys)];
+get_notices_by_keys([]) ->
     [].
--- src/rest_handler.erl
+++ src/rest_handler.erl
@@ -13,23 +13,18 @@
 -export([handle/1]).
 
 handle({get, "notice/", Req}) ->
-    {ok, RedisConnection} = eredis:start_link(),
-    {ok, Keys} = eredis:q(RedisConnection, ["KEYS" | ["notice:*"]]),
 
     Req:respond({
         200,
         [{"Content-Type", "application/json"}],
-        mochijson2:encode(notice:get_notices(Keys, RedisConnection))
+        mochijson2:encode(notice:get_all())
     });
 
 handle({get, "notice/" ++ NoticeUuid, Req}) ->
-    {ok, RedisConnection} = eredis:start_link(),
-    {ok, Value} = eredis:q(RedisConnection, ["GET", "notice:" ++ NoticeUuid]),
-
     Req:respond({
         200,
         [{"Content-Type", "application/json"}],
-        mochijson2:encode([{<<"notice">>, Value}, {<<"uuid">>, list_to_binary(NoticeUuid)}])
+        mochijson2:encode(notice:get({notice_uuid,NoticeUuid}))
     });
 
 handle({get, "user/" ++ Uuid, Req}) ->
@@ -62,21 +57,24 @@ handle({post, "user", Req}) ->
     });
 
 handle({post, "notice/", Req}) ->
-    QueryStringData = Req:parse_qs(),
-    Text = proplists:get_value("text", QueryStringData, "tets"),
-    {ok, RedisConnection} = eredis:start_link(),
-    NoticeUuid = uuid:to_string(uuid:uuid4()),
-    {ok, <<"OK">>} = eredis:q(RedisConnection, ["SET", "notice:" ++ NoticeUuid, Text]),
+    QueryStringData = Req:parse_post(),
+    %% [{"datetime",Datetime},{"group",Group,{"notice",Text}] = QueryStringData,
+
+    Text = proplists:get_value("notice",QueryStringData),
+    Datetime = proplists:get_value("datetime",QueryStringData),
+    Group = proplists:get_value("group",QueryStringData),
+
+    NoticeUuid = notice:add({notice, Group, Datetime, Text}),
 
     Req:respond({
         200,
         [{"Content-Type", "application/json"}],
-        mochijson2:encode([{<<"ok">>, <<"ok">>}, {<<"uuid">>, list_to_binary(NoticeUuid)}])
+        mochijson2:encode([{ok,ok}, {notice_uuid, NoticeUuid}])
     });
 
 handle({_, _, Req}) ->
     Req:respond({
-        200,
+        404,
         [{"Content-Type", "application/json"}],
         mochijson2:encode([{<<"error">>, <<"unknown method">>}])
     }).
