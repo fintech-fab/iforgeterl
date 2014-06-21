742f663f84f82153e545d0536dca75e67198e8bd
Michael Masin <masin_m@itar-tass.com>
2014-06-21 12:08:22 +0400

    handlers updates

--- src/iforget_web.erl
+++ src/iforget_web.erl
@@ -11,53 +11,42 @@
 
 start(Options) ->
     {DocRoot, Options1} = get_option(docroot, Options),
-    Loop = fun (Req) ->
-                   ?MODULE:loop(Req, DocRoot)
-           end,
+    Loop = fun(Req) ->
+        ?MODULE:loop(Req, DocRoot)
+    end,
     mochiweb_http:start([{name, ?MODULE}, {loop, Loop} | Options1]).
 
 stop() ->
     mochiweb_http:stop(?MODULE).
 
 loop(Req, DocRoot) ->
-    "/" ++ Path = Req:get(path),
+        "/" ++ Path = Req:get(path),
     try
         case Req:get(method) of
             'GET' ->
                 case Path of
                     "api/" ++ ApiMethod ->
-                      rest_handler:handle({get,ApiMethod,Req});
+                        rest_handler:handle({get, ApiMethod, Req});
                     _ ->
                         Req:serve_file(Path, DocRoot)
                 end;
             'POST' ->
                 case Path of
-                %%                "api/" ++ ApiMethod ->
-%%                  rest_handler:handle({post,ApiMethod,Req})
-%%              end;
-
-                  "api/message" ->
-                      QueryStringData = Req:parse_qs(),
-                      Text = proplists:get_value("text", QueryStringData,"tets"),
-                      {ok, C} = eredis:start_link(),
-                      MessageUuid = uuid:to_string(uuid:uuid4()),
-                      {ok, <<"OK">>} = eredis:q(C, ["SET", "message:"++MessageUuid,Text ]),
-
-                      Req:respond({200, [{"Content-Type", "text/plain"}],"ok"});
-
+                    "api/" ++ ApiMethod ->
+                        rest_handler:handle({post, ApiMethod, Req});
                     _ ->
-                      header:send({error,Req})
+                        header:send({error, Req})
                 end
-          end
+        end
     catch
         Type:What ->
             Report = ["web request failed",
-                      {path, Path},
-                      {type, Type}, {what, What},
-                      {trace, erlang:get_stacktrace()}],
+                {path, Path},
+                {type, Type}, {what, What},
+                {trace, erlang:get_stacktrace()}],
             error_logger:error_report(Report),
             %% NOTE: mustache templates need \ because they are not awesome.
-            header:send({error,Req})
+            header:send({error, Req})
     end.
 
 %% Internal API
