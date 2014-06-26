ba75355e09e62e8568fe013f7b567a3ecd7a7749
Andrey Perepechaev <perepechaev@inbox.ru>
2014-06-26 22:05:25 +0400

    Отказ от шаблонизатора erlydtl

--- src/render.erl
+++ src/render.erl
@@ -5,14 +5,7 @@
 %% -export([]).
 
 render_ok(Req, TemplateModule, Params, Options) ->
-
-%%     Options = [{header, notice_dtl}];
-    {ok, Header} = ?MODULE:getHeader(Options),
-
-    {ok, Output} = TemplateModule:render(Params),
-    {ok, Layout} = layout_dtl:render([{content, Output}, {header, Header}, {leftsize, 1}, {rightsize, 1}, {mainsize, 10}], [{auto_escape, nil}]),
-    % Here we use mochiweb_request:ok/1 to render a reponse
-    Req:ok({"text/html", Layout}).
+    render_file(Req, TemplateModule).
 
 render_ok(Req, TemplateModule, Params) ->
     render_ok(Req, TemplateModule, Params, []).
@@ -20,6 +13,14 @@ render_ok(Req, TemplateModule, Params) ->
 
 render_ok(Req, TemplateModule) ->
     render_ok(Req, TemplateModule, [], []).
+
+render_file(Req, send_form_dtl) ->
+    Req:serve_file("index.html", "wwwroot");
+
+render_file(Req, TemplateModule) ->
+    io:write(TemplateModule),
+    FileName = erlang:atom_to_list(TemplateModule) ++ ".html",
+    Req:serve_file(FileName, "wwwroot").
 
 getHeader(Options) ->
     HeaderName = lists:keyfind(header, 1, Options),
