72fedbc7f2633449f0458555ac1c46704a508f86
Andrey Perepechaev <a.perepechaev@fintech-fab.ru>
2014-07-03 19:37:18 +0400

    Маскирование телефона

--- src/view/lib/tags/user_tag.erl
+++ src/view/lib/tags/user_tag.erl
@@ -11,7 +11,7 @@
 -behaviour(erlydtl_library).
 
 %% API
--export([user_name/1, version/0, inventory/1]).
+-export([user_name/1, mask_phone/1, mask_email/1, version/0, inventory/1]).
 
 %% --------------------------------------------------------------------
 %% Definitions
@@ -20,11 +20,24 @@
 
 version() -> 1.
 inventory(tags) -> [];
-inventory(filters) -> [user_name].
+inventory(filters) -> [user_name, mask_email, mask_phone].
 
 user_name(UserKey) ->
     User = user:get(UserKey),
     Username = proplists:get_value(username, User),
     Username.
+
+mask_phone(_Phone) ->
+    "".
+
+mask_email(Email) ->
+    case re:run(Email, "^[\\w_]+@") of
+        {match, _} ->
+            MaskEmail = re:replace(Email, "^(\\w{0,2})([\\w_\\d]+)([\\w\\d_]{1})@", "\\1***\\3@", [{return,list}]),
+            MaskEmail;
+        nomatch ->
+            Email
+    end.
+
 
 
